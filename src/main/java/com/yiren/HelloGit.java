828cca7cb08511bc737a906011b7694912a17567
yiren <123.com>
2022-09-11 19:12:54 +0800

    first commit

---
+++ src/main/java/com/yiren/HelloGit.java
@@ -0,0 +1,15 @@
+package com.yiren;
+
+/**
+ * @author 易人
+ * @version V1.0
+ * @Package IntelliJ IDEA
+ * @date 2022/9/11 19:09
+ * @Description:
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git 111111111");
+    }
+}
