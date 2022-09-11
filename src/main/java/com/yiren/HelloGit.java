d7e0694dd3b7575f9014a53bcc9df6aa6a46d58e
yiren <123.com>
2022-09-11 19:24:33 +0800

    master commit2

--- src/main/java/com/yiren/HelloGit.java
+++ src/main/java/com/yiren/HelloGit.java
@@ -14,6 +14,7 @@ public class HelloGit {
         System.out.println("hello git 222222222");
         System.out.println("hello git 333333333");
         System.out.println("hello git 444444444 master");
+        System.out.println("hello git 444444444 master");
 
 
     }
